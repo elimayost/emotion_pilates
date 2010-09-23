0b49bd5508f9affa79905e074bc49e3367937a16
Eli Mayost <elimayost@elimayost-laptop.(none)>
2010-09-23 19:12:20 +0100

    Added paperclip validations to workouts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,11 @@
 class Workout < ActiveRecord::Base
 
 	# paperclip settings
-	has_attached_file :mp3_intro
+	has_attached_file :mp3_intro, :path => ":rails_root/public/uploads/mp3s/:basename.:extension"
+
+	# paperclip validation
+	validates_attachment_size :mp3_intro, :less_than => 10.megabytes, :message => "must be smaller than 10Mb"
+	validates_attachment_content_type :mp3_intro, :content_type => ['audio/mpeg3', 'audio/x-mpeg-3', 'audio/mpeg', 'video/mpeg', 'video/x-mpeg'], :message => "is invalid (must be MP3 file)"
 
   validates :title, :presence => true
   validates :description, :presence => true
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100923131359) do
+ActiveRecord::Schema.define(:version => 20100923140937) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
