c00d7e1b0c396f65b783d77d3bc891e3128940f3
Eli Mayost <elimayost@localhost.(none)>
2010-09-27 14:58:46 +0100

    Added gmail functionality

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,15 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module EmotionPilates
   class Application < Rails::Application
-    config.action_mailer.default_url_options = { :host => 'emotionalpilates.heroku.com' }
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => 'eli.mayost@googlemail.com',
+      :password => '015879620',
+      :authentication => 'plain',
+      :enable_starttls_auto => true }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
