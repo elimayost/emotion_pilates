9fce6be386e68dbc918566cb35e30e386417e60a
Eli Mayost <elimayost@localhost.(none)>
2010-10-01 16:59:46 +0100

    Added disable upload buttons on workouts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
 //Put the jQuery code here
 
+$(function(){
+
+    $('input').attr('disabled', false);
+    $('.cancel_upload_link').hide();
+
+    $('input').focusin(function(){
+        $('input').attr('disabled', true);
+        $(this).attr('disabled', false);
+
+        if($(this).attr('id') == 'workout_mp3_part_one'){
+            $('part_one .cancel_upload_link').show();
+        }else if($(this).attr('id') == 'workout_mp3_part_two'){
+            $('part_two .cancel_upload_link').show();
+        }else if($(this).attr('id') == 'workout_mp3_part_three'){
+            $('part_three .cancel_upload_link').show();
+        }else if($(this).attr('id') == 'workout_mp3_intro'){
+            $('intro .cancel_upload_link').show();
+        }
+
+    });
+
+    $('.cancel_upload_link').click(function(){
+        $('input').attr('disabled', false);
+        $('.cancel_upload_link').hide();
+    });
+
+});
+
